3b19f6271b4b63562b67fd5df6b381cd7a5437a0
RazorWinds <logan.marshall.dev@gmail.com>
2024-06-11 14:22:32 +0100

    Finished Delete pets, updated util

--- API_Test_Framework/src/test/java/com/sparta/PetApi/PetTests/PostNewPetToStore.java
+++ API_Test_Framework/src/test/java/com/sparta/PetApi/PetTests/PostNewPetToStore.java
@@ -1,14 +1,17 @@
 package com.sparta.PetApi.PetTests;
 
+import com.sparta.PetApi.AbstractApiTests;
 import com.sparta.PetApi.AppConfig;
 import com.sparta.PetApi.Pojos.Category;
 import com.sparta.PetApi.Pojos.Pet;
 import com.sparta.PetApi.Pojos.TagsItem;
+import com.sparta.PetApi.utilities.PetUtils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.json.simple.JSONObject;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,47 +19,23 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PostNewPetToStore extends AbstractApi {
+public class PostNewPetToStore extends AbstractApiTests {
     private static Response response;
     private static JSONObject responseBody;
-    private static final String BASE_URI = AppConfig.getBaseUri();
-    private static final String POST_PATH = AppConfig.getPetPath();
+    private static int ID;
+
     private static Pet postBody;
 
     @BeforeAll
     public static void beforeAll() {
-        Category category = new Category();
-        category.setId(1);
-        category.setName("Dogs");
+        Pet pet = PetUtils.createPetPOJO();
+        ID = pet.getId();
+        response = PetUtils.addPet(pet);
+    }
 
-        TagsItem tag = new TagsItem();
-        tag.setId(0);
-        tag.setName("string");
-
-        List<String> photoUrls = new ArrayList<>();
-        photoUrls.add("string");
-
-        List<TagsItem> tags = new ArrayList<>();
-        tags.add(tag);
-
-        Pet pet = new Pet();
-        pet.setId(10);
-        pet.setName("doggie");
-        pet.setCategory(category);
-        pet.setPhotoUrls(photoUrls);
-        pet.setTags(tags);
-        pet.setStatus("available");
-
-        response =
-                RestAssured
-                        .given(PetUtils.postRequestAddPet(
-                                BASE_URI,
-                                POST_PATH,
-                                pet
-                        ))
-                        .when()
-                        .post()
-                        .thenReturn();
+    @AfterAll
+    static void afterAll(){
+        PetUtils.deletePet(ID);
     }
 
     @Test
