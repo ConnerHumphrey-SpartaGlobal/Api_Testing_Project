57f60840704354cb6f78246bc84c5b48d95bf721
Tomasz Wolak <tom.wolak@outlook.com>
2024-06-11 11:31:46 +0100

    FindPetByIDTests

--- API_Test_Framework/src/test/java/com/sparta/PetApi/PetTests/FindPetByIdTests.java
+++ API_Test_Framework/src/test/java/com/sparta/PetApi/PetTests/FindPetByIdTests.java
@@ -1,36 +1,65 @@
 package com.sparta.PetApi.PetTests;
 
-import com.sparta.PetApi.AppConfig.*;
+import com.sparta.PetApi.AbstractApi;
+import com.sparta.PetApi.AppConfig;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-public class FindPetByIdTests {
+public class FindPetByIdTests extends AbstractApi {
 
     private static Response response;
     private static JSONObject responseBody;
-    private static int petID = 10;
+    private static Response invalidResponse;
+    private static JSONObject invalidResponseBody;
+
+    private static int validPetID = 10;
+    private static String invalidPetID = "sheesh101";
+
     @BeforeAll
     static void beforeAll(){
-        response = RestAssured.given().when().get().thenReturn();
+        response = RestAssured.given(getPetByIDSpecification(AppConfig.getBaseUri(), AppConfig.getPetByIdPath(), AppConfig.getToken(), validPetID))
+                .when().get().thenReturn();
+        responseBody = parseResponseToJsonObject(response);
+
+        invalidResponse = RestAssured.given(getPetByIDSpecification(AppConfig.getBaseUri(), AppConfig.getPetByIdPath(), AppConfig.getToken(), invalidPetID))
+                .when().get().thenReturn();
+        invalidResponseBody = parseResponseToJsonObject(invalidResponse);
     }
 
-    public static RequestSpecification getSpecificGithubCommentRequestSpec(String baseUri, String path, String token, int petID) {
+    public static <T> RequestSpecification getPetByIDSpecification(String baseUri, String path, String token, T petID) {
         return new RequestSpecBuilder()
                 .setBaseUri(baseUri)
                 .setBasePath(path)
                 .addHeaders(Map.of(
-                        "Authorization", "Bearer " + token
+                        "Authorization", token
                 ))
                 .addPathParams(Map.of(
-                        "comment_id", petID
+                        "petID", petID
                 ))
                 .build();
     }
+
+
+   @Test
+   @DisplayName("Given a valid ID pet/{petID} path returns pet by id successfuly, with a code 200")
+   void testValidIdPetByID(){
+       MatcherAssert.assertThat(response.statusCode(), Matchers.is(200));
+   }
+
+   @Test
+   @DisplayName("Given an invalid ID pet/{petID} path returns a 400 satus code")
+    void testInvalidIdPetByID(){
+        MatcherAssert.assertThat(invalidResponse.statusCode(), Matchers.is(400));
+   }
 
 }
