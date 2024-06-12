6b11a759c6462a2d053525d2c0d0f6249fb1186f
RazorWinds <logan.marshall.dev@gmail.com>
2024-06-12 10:54:53 +0100

    Final mock commit

--- API_Test_Framework/src/test/java/com/sparta/PetApi/utilityTests/PetUtilsTest.java
+++ API_Test_Framework/src/test/java/com/sparta/PetApi/utilityTests/PetUtilsTest.java
@@ -3,9 +3,13 @@ package com.sparta.PetApi.utilityTests;
 import com.sparta.PetApi.Pojos.Pet;
 import com.sparta.PetApi.utilities.PetUtils;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -29,16 +33,33 @@ public class PetUtilsTest {
         Response response = PetUtils.deletePet(petId);
         assertNotNull(response);
     }
+    
 
     @Test
-    public void testUpdatePetName() {
-        int petId = 10;
-        String newName = "newName";
-        Response mockedResponse = mock(Response.class);
-        PetUtils utilsSpy = Mockito.spy(new PetUtils());
-        doReturn(mockedResponse).when(utilsSpy).updatePetName(petId, newName);
+    @DisplayName("Pet not existing returns false")
+    void isPetFoundReturnsFalse(){
+        ResponseBody mockBody = mock(ResponseBody.class);
+        Response mockResponse = mock(Response.class);
+        when(mockResponse.getBody()).thenReturn(mockBody);
+        when(mockBody.asString()).thenReturn("Pet not found");
 
-        Response response = PetUtils.updatePetName(petId, newName);
-        assertNotNull(response);
+        assertThat(PetUtils.isPetFound(mockResponse), is(false));
+    }
+
+    @Test
+    @DisplayName("Name is updated correctly and not dependant on the original response")
+    void updateNameIsCorrect(){
+        ResponseBody mockBody = mock(ResponseBody.class);
+        when(mockBody.as(Pet.class)).thenReturn(PetUtils.createPetPOJO());
+
+        Response mockResponse = mock(Response.class);
+        when(mockResponse.getBody()).thenReturn(mockBody);
+
+
+        Response finalResponse = PetUtils.updatePetName(mockResponse,"TestName");
+
+        Pet returnedPet = finalResponse.getBody().as(Pet.class);
+
+        assertThat(returnedPet.getName(), is("TestName"));
     }
 }
